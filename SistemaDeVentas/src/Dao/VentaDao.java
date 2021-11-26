cedde028529da4abb9fcf028db46b71e55e4b248
Manuel Chunca Mamani <Manuel Chunca Mamani@DESKTOP-H8TLSTD>
2021-11-26 11:43:59 -0500

    Sistema de Ventas con IGU

---
+++ SistemaDeVentas/src/Dao/VentaDao.java
@@ -0,0 +1,138 @@
+package Dao;
+
+import Modelo.Conexion;
+import Modelo.DetalleTo;
+import Modelo.VentaTo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VentaDao {
+
+    Connection con;
+    Conexion cn = new Conexion();
+    PreparedStatement ps;
+    ResultSet rs;
+    int r;
+
+    public int IdVenta() {
+        int id = 0;
+        String sql = "SELECT MAX(id) FROM ventas";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return id;
+    }
+
+    public int RegistrarVenta(VentaTo v) {
+        String sql = "INSERT INTO ventas (cliente, vendedor, total, fecha) VALUES (?,?,?,?)";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, v.getCliente());
+            ps.setString(2, v.getVendedor());
+            ps.setDouble(3, v.getTotal());
+            ps.setString(4, v.getFecha());
+            ps.execute();
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+        return r;
+    }
+
+    public int RegistrarDetalle(DetalleTo Dv) {
+        String sql = "INSERT INTO detalle (id_pro, cantidad, precio, id_venta) VALUES (?,?,?,?)";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, Dv.getId_pro());
+            ps.setInt(2, Dv.getCantidad());
+            ps.setDouble(3, Dv.getPrecio());
+            ps.setInt(4, Dv.getId());
+            ps.execute();
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+        return r;
+    }
+
+    public boolean ActualizarStock(int cant, int id) {
+        String sql = "UPDATE productos SET stock = ? WHERE id = ?";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cant);
+            ps.setInt(2, id);
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+
+    public List Listarventas() {
+        List<VentaTo> ListaVenta = new ArrayList();
+        String sql = "SELECT c.id AS id_cli, c.nombre, v.* FROM clientes c INNER JOIN ventas v ON c.id = v.cliente";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                VentaTo vent = new VentaTo();
+                vent.setId(rs.getInt("id"));
+                vent.setNombre_cli(rs.getString("nombre"));
+                vent.setVendedor(rs.getString("vendedor"));
+                vent.setTotal(rs.getDouble("total"));
+                vent.setFecha(rs.getString("fecha"));
+                ListaVenta.add(vent);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return ListaVenta;
+    }
+
+    public VentaTo BuscarVenta(int id) {
+        VentaTo cl = new VentaTo();
+        String sql = "SELECT * FROM ventas WHERE id = ?";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                cl.setId(rs.getInt("id"));
+                cl.setCliente(rs.getInt("cliente"));
+                cl.setTotal(rs.getDouble("total"));
+                cl.setVendedor(rs.getString("vendedor"));
+                cl.setFecha(rs.getString("fecha"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return cl;
+    }
+}
